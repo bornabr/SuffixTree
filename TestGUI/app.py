ae54ad4e2df524d33f8bc07b812f99127fe1f9bf
Borna <bornabarahimi@gmail.com>
2021-02-19 00:04:42 +0330

    complete search pattern api

--- TestGUI/app.py
+++ TestGUI/app.py
@@ -1,14 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
+from suffixTree.suffixTree import SuffixTree
+from suffixTree.helpers.searchPattern import SearchPattern
 
 PORT = 5000
 HOST = '0.0.0.0'
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def home():
-    return render_template('index.html')
-    
+	return render_template('index.html')
+
+
+@app.route('/api/search-pattern', methods=['POST'])
+def search_pattern():
+	strings = request.form.get('strings')
+	if(strings is None or strings == ''):
+		strings = ''
+	strings = strings.split('>\n')
+	tree = SuffixTree(strings, True)
+	pattern = request.form.get('pattern')
+	if(pattern is None or pattern == ''):
+		pattern = ''
+	checker = SearchPattern(tree, pattern)
+	result = {'tree': tree.__dict__(),
+			  'result': checker.search()}
+	print(result)
+	return jsonify(result)
+
 
 if __name__ == '__main__':
-    app.run(host=HOST, port=PORT)
+	app.run(host=HOST, port=PORT)
--- TestGUI/static/script.js
+++ TestGUI/static/script.js
@@ -18,6 +18,33 @@ function openPage(pageName, elmnt, color) {
 	// Add the specific color to the button used to open the tab content
 	elmnt.style.backgroundColor = color;
 }
+$(document).ready(function () {
+	document.getElementById("defaultOpen").click();
 
+	$("#form-section-one").submit(function (e) {
+		e.preventDefault();
+		console.log($("#form-section-one").serializeArray())
+		$.post("/api/search-pattern",
+			$("#form-section-one").serializeArray(),
+			function (res) {
+				console.log(res)
+				tree = res.tree
+				var nodes = new vis.DataSet(tree.nodes);
+	
+				// create an array with edges
+				var edges = new vis.DataSet(tree.edges);
+	
+				// create a network
+				var container = document.getElementById("section1-network");
+				var data = {
+					nodes: nodes,
+					edges: edges,
+				};
+				var options = {};
+				var network = new vis.Network(container, data, options);
+				container.style.display = "block"
+			});
+		
+	});
+});
 // Get the element with id="defaultOpen" and click on it
-document.getElementById("defaultOpen").click();
