0d663a061e3765e3d5a23a6af98f2e36a57ae0e6
Borna <bornabarahimi@gmail.com>
2021-02-18 19:02:38 +0330

    add basic files for testgui and fix docker

--- TestGUI/app.py
+++ TestGUI/app.py
@@ -1,8 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
+PORT = 5000
+HOST = '0.0.0.0'
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return 'Hello World!'
+def home():
+    return render_template('index.html')
+    
+
+if __name__ == '__main__':
+    app.run(host=HOST, port=PORT)
