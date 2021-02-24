4bfb3dffd44c35e6052751f7af328861152a8307
Borna <bornabarahimi@gmail.com>
2021-02-24 12:31:59 +0330

    complete gui for section 1

--- TestGUI/static/script.js
+++ TestGUI/static/script.js
@@ -18,6 +18,45 @@ function openPage(pageName, elmnt, color) {
 	// Add the specific color to the button used to open the tab content
 	elmnt.style.backgroundColor = color;
 }
+
+function search_pattern (data) {
+	$.post("/api/search-pattern",
+		data,
+		function (res) {
+			console.log(res)
+			tree = res.tree
+			let nodes = new vis.DataSet(tree.nodes);
+
+			// create an array with edges
+			let edges = new vis.DataSet(tree.edges);
+
+			// create a network
+			let container = document.getElementById("section1-network");
+			let data = {
+				nodes: nodes,
+				edges: edges,
+			};
+			let options = {};
+			let network = new vis.Network(container, data, options);
+			container.style.display = "block"
+
+			result = res.result
+			$('#section1-result').append(`<pre>${JSON.stringify(res.result, null, 2)}</pre>`)
+			$('#section1-result').show()
+			try {
+				const link = document.createElement('a');
+				link.href = window.URL.createObjectURL(new Blob(['\ufeff', JSON.stringify(result, null, 2)], { type: 'text/plain' }));
+				link.setAttribute('download', 'result');
+				document.body.appendChild(link);
+				link.click();
+				document.body.removeChild(link);
+			} catch(err) {
+				console.log(err)
+			}
+			
+		});
+}
+
 $(document).ready(function () {
 	document.getElementById("defaultOpen").click();
 
@@ -26,34 +65,35 @@ $(document).ready(function () {
 		$('#section1-result').empty()
 		$('#section1-result').hide()
 		$('#section1-network').hide()
-		serialize = $("#form-section-one").serializeArray()
-		if (!serialize.find(item => item.name == 'strings').value && serialize.find(item => item.name == 'strings').value == '')
-			return
-		$.post("/api/search-pattern",
-			$("#form-section-one").serializeArray(),
-			function (res) {
-				console.log(res)
-				tree = res.tree
-				let nodes = new vis.DataSet(tree.nodes);
-	
-				// create an array with edges
-				let edges = new vis.DataSet(tree.edges);
-	
-				// create a network
-				let container = document.getElementById("section1-network");
-				let data = {
-					nodes: nodes,
-					edges: edges,
-				};
-				let options = {};
-				let network = new vis.Network(container, data, options);
-				container.style.display = "block"
-				
-				result = res.result
-				$('#section1-result').append(`<pre>${JSON.stringify(res.result, null, 2)}</pre>`)
-				$('#section1-result').show()
-			});
-		
+		const serialized = $("#form-section-one").serializeArray()
+		const data = {}
+		serialized.forEach(({ name, value }) => data[name] = value);
+		if (!data.strings || data.strings == ''){
+			if (!window.File || !window.FileReader || !window.FileList || !window.Blob) {
+				alert('The File APIs are not fully supported in this browser.');
+				return;
+			}
+			let inputFile = document.getElementById('file-input');
+			if (!inputFile) {
+				alert("Um, couldn't find the fileinput element.");
+			}
+			else if (!inputFile.files) {
+				alert("This browser doesn't seem to support the `files` property of file inputs.");
+			}
+			else if (!inputFile.files[0]) {
+				alert("Please select a file before submiting");
+			}
+			else {
+				let file = inputFile.files[0];
+				let fr = new FileReader();
+				fr.readAsText(file);
+				fr.onload = () => {
+					data.strings = fr.result
+					search_pattern(data)
+				}
+			}
+		} else {
+			search_pattern(data)
+		}
 	});
 });
-// Get the element with id="defaultOpen" and click on it
